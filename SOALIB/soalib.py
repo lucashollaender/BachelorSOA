e8e9da37c70038382be18215b175f1d1043eac12
Mrichter02 <markusrichterhansen@gmail.com>
2026-02-02 13:15:50 +0100

    12

--- SOALIB/soalib.py
+++ SOALIB/soalib.py
@@ -1,1 +1,11 @@
 # Soa Library
+import numpy as np
+
+
+def skew(z):
+    z = np.asarray(z).reshape(3,)
+    return np.array([
+        [0.0,    -z[2],  z[1]],
+        [z[2],    0.0,  -z[0]],
+        [-z[1],   z[0],  0.0]
+    ])
