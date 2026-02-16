d80926cf40f1888f73e8b4ed80109a6bdf1ccec7
JeppeHjorth <jepp6479@gmail.com>
2026-02-16 17:10:34 +0100

    External fil i SOALIB kan nu kaldes overalt

--- SOALIB/RigidForwardSOA.py
+++ SOALIB/RigidForwardSOA.py
@@ -581,7 +581,11 @@ class Simulation:
         else:
             plt.show()
 
-""" ------ Body setup ------ """
+""" ------ File Setup ------ """
+# Remember to run this:
+# from SOALIB.RigidForwardSOA import Joint, Inertia, SOABody, MultibodySystem, Simulation
+
+""" ------ Body Setup ------ """
 # *** Body Parameters ****
 # klOO:     Hinge position (row vector)
 # H_type:   Hinge type (string)
@@ -655,48 +659,3 @@ class Simulation:
 #               file_path = r"C:\Users\jepp6\OneDrive..."
 #               Choose another folder than the GIT-Hub synchronize folder, since the file will
 #               be to big and result in a "commit" error.
-
-klOO = np.array([0, 0, 5])
-H_type1 = "spherical"
-H_type2 = "spherical"
-H_type3 = "spherical"
-
-m = 1
-CkJk = np.array([1, 1, 0.1])
-klOC = np.array([0, 0, 2.5])
-
-j1 = Joint(klOO, H_type1)
-i = Inertia(m, CkJk, klOC)
-b1 = SOABody(j1, i)
-
-j2 = Joint(klOO, H_type2)
-i = Inertia(m, CkJk, klOC)
-b2 = SOABody(j2, i)
-
-j3 = Joint(klOO, H_type3)
-i = Inertia(m, CkJk, klOC)
-b3 = SOABody(j3, i)
-
-b1.set_initial_theta0(sb.get_quat_from_degrees(10, -25, 0))
-b2.set_initial_theta0(sb.get_quat_from_degrees(-135, 0, 0))
-
-b1.set_initial_beta0(np.array([0, 0 , 0]).reshape(3, 1))
-
-bodies = [b1, b2]
-
-system = MultibodySystem(bodies)
-
-tf = 10
-dt = 0.01
-
-sim = Simulation(system, tf, dt)
-
-sim.camera_speed(0.1)
-
-sim.IntegrateSystem()
-
-save_dir = r"C:\Users\jepp6\OneDrive - Aarhus universitet\Dokumenter\Noter\6. Semester\Bachelor Projekt\BachelorCode\Renders"
-
-sim.animate()
-
-#sim.animate("123", save_dir)
