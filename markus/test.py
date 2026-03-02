b39fe85f7764a1ce13569f8c10406778a6d0af41
Mrichter02 <markusrichterhansen@gmail.com>
2026-03-02 09:44:17 +0100

    d

--- markus/test.py
+++ markus/test.py
@@ -1,3 +1,7 @@
+from Flex_Point_Mass.SOABody import SOABody
+from Flex_Point_Mass.MultibodySystem import MultibodySystem
+from Flex_Point_Mass.Simulation import Simulation
+from Flex_Point_Mass.Body_Properties import Joint, Rigid_Properties, Flex_Properties
 import numpy as np
 from SOALIB import soalib as sb
-import BachelorSOA.Flex_Point_Mass.Structural_Analysis_PM_Rect as sa
+import pandas as pd
