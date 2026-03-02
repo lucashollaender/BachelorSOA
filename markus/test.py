cc5046c8869ac234b090175be4f16c2c0a1f4a28
Mrichter02 <markusrichterhansen@gmail.com>
2026-03-02 09:28:59 +0100

    g

--- markus/test.py
+++ markus/test.py
@@ -1,5 +1,3 @@
 import numpy as np
 from SOALIB import soalib as sb
-from BachelorSOA.Flex_Point_Mass import Structural_Analysis_PM_Rect
-
-fem = Structural_Analysis_PM_Rect()
+import BachelorSOA.Flex_Point_Mass.Structural_Analysis_PM_Rect as sa
